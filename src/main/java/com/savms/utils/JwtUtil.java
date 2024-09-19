14e1f60ee877c46d0cdcfb55eb9de19f547f109e
Yifeng_Wei <u7727744@anu.edu.au>
2024-09-20 09:02:35 +1000

    Update JwtUtil to make the system safer

---
+++ src/main/java/com/savms/utils/JwtUtil.java
@@ -0,0 +1,28 @@
+package com.savms.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import java.util.Date;
+import java.util.Map;
+
+public class JwtUtil {
+
+    private static final String KEY = "SCUAM2024";
+
+    public static String genToken(Map<String, Object> claims) {
+        return JWT.create()
+                .withClaim("claims", claims)
+                .withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 60 * 60 ))
+                .sign(Algorithm.HMAC256(KEY));
+    }
+
+    public static Map<String, Object> parseToken(String token) {
+        return JWT.require(Algorithm.HMAC256(KEY))
+                .build()
+                .verify(token)
+                .getClaim("claims")
+                .asMap();
+    }
+
+}
