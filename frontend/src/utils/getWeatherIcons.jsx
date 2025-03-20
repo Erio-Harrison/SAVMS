c42e44dcf8c62cf601d402581bfcf2e1ee7e7ef0
Yikun <u7764107@anu.edu.au>
2025-03-20 15:40:23 +1100

    SVG updates

--- frontend/src/utils/getWeatherIcons.jsx
+++ frontend/src/utils/getWeatherIcons.jsx
@@ -1,9 +1,18 @@
-import sunnySvg from "../assets/weather/sunny.svg";
-import cloudySvg from "../assets/weather/cloudy.svg";
+import sunnySvg from "../assets/weather/sunnyDay.svg";
+import mistDaySvg from "../assets/weather/mistDay.svg";
+import mistNight from "../assets/weather/mistNight.svg";
+import partlyCloudyDaySvg from "../assets/weather/partlyCloudyDay.svg";
+import partlyCloudyNightSvg from "../assets/weather/partlyCloudyNight.svg";
+import cloudyDaySvg from "../assets/weather/cloudyDay.svg";
+import cloudyNightSvg from "../assets/weather/cloudyNight.svg";
+import overcastSvg from "../assets/weather/overcast.svg";
 import rainy1Svg from "../assets/weather/rainy-1.svg";
 import snowy1Svg from "../assets/weather/snowy-1.svg";
-import rainy2Svg from "../assets/weather/rainy-2.svg";
-import snowy2Svg from "../assets/weather/snowy-2.svg";
+import patchyRainPossibleDaySvg from "../assets/weather/patchyRainPossibleDay.svg";
+import patchyRainPossibleNightSvg from "../assets/weather/patchyRainPossibleNight.svg";
+import patchySnowPossibleDaySvg from "../assets/weather/patchySnowPossibleDay.svg";
+import patchySnowPossibleNightSvg from "../assets/weather/patchySnowPossibleNight.svg";
+import sleetSvg from "../assets/weather/sleet.svg";
 import rainy3Svg from "../assets/weather/rainy-3.svg";
 import snowy3Svg from "../assets/weather/snowy-3.svg";
 import rainy4Svg from "../assets/weather/rainy-4.svg";
@@ -11,12 +20,10 @@ import snowy4Svg from "../assets/weather/snowy-4.svg";
 import rainy5Svg from "../assets/weather/rainy-5.svg";
 import snowy5Svg from "../assets/weather/snowy-5.svg";
 import rainy6Svg from "../assets/weather/rainy-6.svg";
-import snowy6Svg from "../assets/weather/snowy-6.svg";
+import blowingSnowSvg from "../assets/weather/blowingSnow.svg";
 import thunderSvg from "../assets/weather/thunder.svg";
 import weatherSvg from "../assets/weather/weather.svg";
-import partlyCloudyDay from "../assets/weather/cloudy-day-2.svg";
-import partlyCloudyNight from "../assets/weather/cloudy-night-2.svg";
-import nightSvg from "../assets/weather/night.svg";
+import nightSvg from "../assets/weather/nightClear.svg";
 
 export function getWeatherIcon(weather, day) {
     if(day){
@@ -24,31 +31,31 @@ export function getWeatherIcon(weather, day) {
             case 1000:
                 return sunnySvg;
             case 1003:
-                return partlyCloudyDay;
+                return partlyCloudyDaySvg;
             case 1006:
-                return cloudySvg;
+                return cloudyDaySvg;
             case 1009:
-                return cloudySvg;
+                return overcastSvg;
             case 1030:
-                return cloudySvg;
+                return mistDaySvg;
             case 1063:
-                return rainy2Svg;
+                return patchyRainPossibleDaySvg;
             case 1066:
-                return snowy2Svg;
+                return patchySnowPossibleDaySvg;
             case 1069:
-                return snowy2Svg;
+                return sleetSvg;
             case 1072:
-                return rainy2Svg;
+                return patchyRainPossibleDaySvg;
             case 1087:
                 return thunderSvg;
             case 1114:
-                return snowy6Svg;
+                return blowingSnowSvg;
             case 1117:
-                return snowy6Svg;
+                return blowingSnowSvg;
             case 1135:
-                return cloudySvg;
+                return cloudyDaySvg;
             case 1147:
-                return cloudySvg;
+                return cloudyDaySvg;
             case 1150:
                 return rainy4Svg;
             case 1153:
@@ -86,9 +93,9 @@ export function getWeatherIcon(weather, day) {
             case 1219:
                 return snowy5Svg;
             case 1222:
-                return snowy6Svg;
+                return blowingSnowSvg;
             case 1225:
-                return snowy6Svg;
+                return blowingSnowSvg;
             case 1237:
                 return rainy4Svg;
             case 1240:
@@ -125,31 +132,31 @@ export function getWeatherIcon(weather, day) {
             case 1000:
                 return nightSvg;
             case 1003:
-                return partlyCloudyNight;
+                return partlyCloudyNightSvg;
             case 1006:
-                return cloudySvg;
+                return cloudyNightSvg;
             case 1009:
-                return cloudySvg;
+                return overcastSvg;
             case 1030:
-                return cloudySvg;
+                return mistNight;
             case 1063:
-                return rainy2Svg;
+                return patchyRainPossibleNightSvg;
             case 1066:
-                return snowy2Svg;
+                return patchySnowPossibleNightSvg;
             case 1069:
-                return snowy2Svg;
+                return sleetSvg;
             case 1072:
-                return rainy2Svg;
+                return patchyRainPossibleDaySvg;
             case 1087:
                 return thunderSvg;
             case 1114:
-                return snowy6Svg;
+                return blowingSnowSvg;
             case 1117:
-                return snowy6Svg;
+                return blowingSnowSvg;
             case 1135:
-                return cloudySvg;
+                return cloudyNightSvg;
             case 1147:
-                return cloudySvg;
+                return cloudyNightSvg;
             case 1150:
                 return rainy4Svg;
             case 1153:
@@ -187,9 +194,9 @@ export function getWeatherIcon(weather, day) {
             case 1219:
                 return snowy5Svg;
             case 1222:
-                return snowy6Svg;
+                return blowingSnowSvg;
             case 1225:
-                return snowy6Svg;
+                return blowingSnowSvg;
             case 1237:
                 return rainy4Svg;
             case 1240:
