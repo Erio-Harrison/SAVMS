cfd6c71cfe73c6366f0f7e07d1dc0d9147a39b5c
Erio-Harrison <u7541840@gmail.com>
2025-03-25 14:49:59 +1100

    update cpp file's name

--- engine/src/handleData/handleData.h
+++ engine/src/handleData/handleData.h
@@ -1,13 +1,13 @@
-#ifndef DATA_PROCESSOR_H
-#define DATA_PROCESSOR_H
+#ifndef HANDLE_DATA_H
+#define HANDLE_DATA_H
 
 #include <vector>
 #include <string>
 
-class DataProcessor {
+class HandleData {
 public:
-    DataProcessor();
-    ~DataProcessor();
+    HandleData();
+    ~HandleData();
 
     std::vector<std::string> processData(const std::vector<std::string>& rawData);
 
@@ -15,4 +15,4 @@ private:
     // 添加任何必要的私有成员
 };
 
-#endif // DATA_PROCESSOR_H
+#endif
