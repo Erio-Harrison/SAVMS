4ea43d16ffc34d3f33dcadc4c2f9df0ec8505a44
RichardQfz <richardcheng2001@outlook.com>
2025-04-15 13:49:19 +1000

    Update Vehicle class Create VehicleDataFecher class to read in online vehicle data

--- src/main/java/com/savms/entity/Vehicle.java
+++ src/main/java/com/savms/entity/Vehicle.java
@@ -49,6 +49,30 @@ public class Vehicle
         }
 
         // Getters and Setters
+        public double getLength() {
+            return length;
+        }
+
+        public void setLength(double length) {
+            this.length = length;
+        }
+
+        public double getWidth() {
+            return width;
+        }
+
+        public void setWidth(double width) {
+            this.width = width;
+        }
+
+        public double getHeight() {
+            return height;
+        }
+
+        public void setHeight(double height) {
+            this.height = height;
+        }
+
     }
 
     public static class Sensor
@@ -65,6 +89,22 @@ public class Vehicle
         }
 
         // Getters and Setters
+        public String getModel() {
+            return model;
+        }
+
+        public void setModel(String model) {
+            this.model = model;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        public void setCount(int count) {
+            this.count = count;
+        }
+
     }
 
     public static class Communication
@@ -79,6 +119,14 @@ public class Vehicle
         }
 
         // Getters and Setters
+        public String getIpAddress() {
+            return ipAddress;
+        }
+
+        public void setIpAddress(String ipAddress) {
+            this.ipAddress = ipAddress;
+        }
+
     }
 
     public static class Location
@@ -95,6 +143,22 @@ public class Vehicle
         }
 
         // Getters and Setters
+        public double getLongitude() {
+            return longitude;
+        }
+
+        public void setLongitude(double longitude) {
+            this.longitude = longitude;
+        }
+
+        public double getLatitude() {
+            return latitude;
+        }
+
+        public void setLatitude(double latitude) {
+            this.latitude = latitude;
+        }
+
     }
 
     // Constructors
@@ -114,6 +178,189 @@ public class Vehicle
     }
 
     // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    public void setCarModel(String carModel) {
+        this.carModel = carModel;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getEnergyType() {
+        return energyType;
+    }
+
+    public void setEnergyType(String energyType) {
+        this.energyType = energyType;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    public Sensor getRadar() {
+        return radar;
+    }
+
+    public void setRadar(Sensor radar) {
+        this.radar = radar;
+    }
+
+    public Sensor getCamera() {
+        return camera;
+    }
+
+    public void setCamera(Sensor camera) {
+        this.camera = camera;
+    }
+
+    public Communication getCommunication() {
+        return communication;
+    }
+
+    public void setCommunication(Communication communication) {
+        this.communication = communication;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public int getLeftoverEnergy() {
+        return leftoverEnergy;
+    }
+
+    public void setLeftoverEnergy(int leftoverEnergy) {
+        this.leftoverEnergy = leftoverEnergy;
+    }
+
+    public int getConnectionStatus() {
+        return connectionStatus;
+    }
+
+    public void setConnectionStatus(int connectionStatus) {
+        this.connectionStatus = connectionStatus;
+    }
+
+    public int getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(int taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+
+    public int getHealthStatus() {
+        return healthStatus;
+    }
+
+    public void setHealthStatus(int healthStatus) {
+        this.healthStatus = healthStatus;
+    }
+
+    public int getEngineRPM() {
+        return engineRPM;
+    }
+
+    public void setEngineRPM(int engineRPM) {
+        this.engineRPM = engineRPM;
+    }
+
+    public double getLubeOilPressure() {
+        return lubeOilPressure;
+    }
+
+    public void setLubeOilPressure(double lubeOilPressure) {
+        this.lubeOilPressure = lubeOilPressure;
+    }
+
+    public double getFuelPressure() {
+        return fuelPressure;
+    }
+
+    public void setFuelPressure(double fuelPressure) {
+        this.fuelPressure = fuelPressure;
+    }
+
+    public double getCoolantPressure() {
+        return coolantPressure;
+    }
+
+    public void setCoolantPressure(double coolantPressure) {
+        this.coolantPressure = coolantPressure;
+    }
+
+    public double getLubeOilTemp() {
+        return lubeOilTemp;
+    }
+
+    public void setLubeOilTemp(double lubeOilTemp) {
+        this.lubeOilTemp = lubeOilTemp;
+    }
+
+    public double getCoolantTemp() {
+        return coolantTemp;
+    }
+
+    public void setCoolantTemp(double coolantTemp) {
+        this.coolantTemp = coolantTemp;
+    }
+
+    public int getEngineCondition() {
+        return engineCondition;
+    }
+
+    public void setEngineCondition(int engineCondition) {
+        this.engineCondition = engineCondition;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 
     @Override
     public String toString()
