1fb33e1b4c1490bcc96cfbc58f21a0c53e725d5b
RichardQfz <richardcheng2001@outlook.com>
2025-05-08 11:29:48 +1000

    User entity: add role (client, admin)

--- src/main/java/com/savms/entity/User.java
+++ src/main/java/com/savms/entity/User.java
@@ -12,6 +12,7 @@ public class User {
     private String email;
     private LocalDateTime createdAt;
     private String password;
+    private int role; // 0 for admin, 1 for client
 
     // Constructors
     public User() {
@@ -65,6 +66,16 @@ public class User {
     public void setPassword( String password )
     {
         this.password = password;
+    }
+
+    public int getRole()
+    {
+        return role;
+    }
+
+    public void setRole(int role)
+    {
+        this.role = role;
     }
 
     @Override
