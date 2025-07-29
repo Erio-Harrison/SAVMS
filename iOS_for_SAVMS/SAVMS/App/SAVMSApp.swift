944c93920f505015cb3a0643f0daf4d28b9c14b0
Yikun <u7764107@anu.edu.au>
2025-07-30 04:05:47 +1000

    save for now

--- iOS_for_SAVMS/SAVMS/App/SAVMSApp.swift
+++ iOS_for_SAVMS/SAVMS/App/SAVMSApp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct SAVMSApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
